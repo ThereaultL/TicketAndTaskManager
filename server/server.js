3c53efdaeb92f0e4c40d568d7255c170a05482fd
Lillian Thereault <lillian05ad@gmail.com>
2025-07-15 21:15:37 -0400

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -3,23 +3,40 @@ const cors = require("cors")
 
 const PORT = 5000;
 
+let tickets = [];
+
 //initilize the server
 const app = express();
 app.use(cors());
 //parsing applicaiton and json
 app.use(express.json());
 
-// app.get("/", () => {
-//     console.log("GET has been called");
-// });
+/**
+ * Gets the list of open tickets
+ */
+app.get("/", (req, res) => {
+    res.json(tickets);
+});
 
-function getTickets() {
-    return null
-}
-
-function createTicket() {
-    return null
-}
+/**
+ * Creates a tickets based on the request body and pushes the new ticket onto the list of 
+ * open tickets
+ */
+app.post("/TicketForm", (req, res) => {
+    // const title = req.body.title;
+    //const description = req.body.title;
+    const {title, description} = req.body; //parsed body from the request
+    let newTicket = {
+        id : tickets.length + 1,
+        title,
+        description,
+        status : "Open"
+    };
+    tickets.push(newTicket);
+    console.log(`Ticket Created: ${title}, ${description}`);
+    //201 HTTP code : Created POST
+    res.status(201).json(newTicket);
+});
 
 function updateTicket() {
     return null
