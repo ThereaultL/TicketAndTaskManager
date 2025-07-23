1164a2127c2442275013851c64c9c602573548a8
Lillian Thereault <lillian05ad@gmail.com>
2025-07-23 09:25:44 -0400

    Add files via upload
    TicketDetail page now reflects a Ticket's properties. No CSS, just plain HTML.

--- client/src/TicketDetail.js
+++ client/src/TicketDetail.js
@@ -1,10 +1,23 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 import "./TicketSummary.css";
 
 function TicketDetail() {
+  const location = useLocation();
+  const ticket = location.state;
+  console.log(ticket);
+
   return (
     <div className="screen-box">
-         <p>DEATILS</p> 
+        <p>DEATILS</p> 
+      
+        <div>
+          <p>{ticket.title}</p>
+          <p>{ticket.description}</p>
+        </div>
+        <div>
+          <p>{ticket.id}</p>
+          <p>{ticket.status}</p>
+        </div>
     </div>
   );
 }
