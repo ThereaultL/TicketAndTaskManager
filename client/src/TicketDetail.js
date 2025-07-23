294ea2cc597e87219400035a9547395954567ddd
Lillian Thereault <lillian05ad@gmail.com>
2025-07-23 09:48:52 -0400

    Add files via upload
    input boxes, update and resolve button added. Currently, no handle submit to prevent deleting test tickets.

--- client/src/TicketDetail.js
+++ client/src/TicketDetail.js
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import "./TicketSummary.css";
 
 function TicketDetail() {
@@ -6,18 +7,63 @@ function TicketDetail() {
   const ticket = location.state;
   console.log(ticket);
 
+  const [title, setTitle] = useState(ticket.title);
+  const [description, setDescription] = useState(ticket.description);
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  }
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  }
+
+  const handleUpdate = async (event) => {
+  
+  }
+
+  const handleResolve = async (event) => {
+    event.preventDefault();
+    const response = await fetch(
+      "/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: ticket.id,
+          title: title,
+          description: description,
+          status: "Resolved", // Assuming you want to set the status to Resolved
+        }),
+      }
+    );
+
+    if (response.ok) {
+      const updatedTicket = await response.json();
+      console.log("Ticket updated:", updatedTicket);
+      // Optionally, you can redirect or update the state here
+    } else {
+      console.error("Failed to update ticket");
+    }
+  }
+
   return (
     <div className="screen-box">
-        <p>DEATILS</p> 
-      
+      <form>
         <div>
-          <p>{ticket.title}</p>
-          <p>{ticket.description}</p>
+          <p>Title:</p>
+          <input value={title} onChange={handleTitleChange} />
+          <p>Description</p>
+          <input value={description} onChange={handleDescriptionChange} />
         </div>
         <div>
-          <p>{ticket.id}</p>
-          <p>{ticket.status}</p>
+          <p>ID: {ticket.id}</p>
+          {/** Want to change into a drop box with status options */}
+          <p>Status: {ticket.status}</p>
         </div>
+        <button type="submit" class="button">Update</button>
+        <button type="submit" class="button">Resolve</button>
+      </form>
     </div>
   );
 }
